d8b2d5ba33e6768fc6acd4622141b94c7e15c86f
MURTADHA ALBAHRANI <160097883+MURTADHA28@users.noreply.github.com>
2024-06-22 09:00:19 +0300

    Update Factorial.php

--- Factorial.php
+++ Factorial.php
@@ -1,1 +1,10 @@
-
+function calculateFactorial($number) {
+    if ($number < 0) {
+        return null;
+    }
+    if ($number === 0) {
+        return 1;
+    }
+    return $number * calculateFactorial($number - 1);
+}
+echo calculateFactorial(4); 
